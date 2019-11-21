3a0e1fb432e572fc6365a1bf0014b31fc7b2845f
Rayeed Anwar <_rayeed@live.com>
2019-11-21 09:05:07 +0000

    Changed help page message

--- src/app.js
+++ src/app.js
@@ -37,7 +37,7 @@ app.get('/about', (req, res) => {
 app.get('/help', (req,res) => {
     res.render('help', {
         title: 'HELP MEH',
-        msg: "What's up?",
+        msg: "What's up? need help?",
         
 
     })
